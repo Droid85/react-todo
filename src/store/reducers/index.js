ee708d5d0f727ac49ddcdafb1e2d62edc0e59872
Andrii Ostapenko <aostapenko@atncorp.com>
2023-01-25 15:57:46 +0200

    added redux to todo app

---
+++ src/store/reducers/index.js
@@ -0,0 +1,27 @@
+import { ACTION_ADD_TODO, ACTION_DELETE_TODO, ACTION_UPDATE_TODO } from '../actions/edit-todos';
+
+const initialState = sessionStorage.getItem('todos') ? JSON.parse(sessionStorage.getItem('todos')) : [
+	{id: 1, title: 'title', isDone: false},
+]
+
+const rootReducer = (state = initialState, {type, payload}) => {
+	switch(type) {
+		case ACTION_ADD_TODO:
+			return [...state, {id: Math.random(), title: payload, isDone: false}]
+		case ACTION_DELETE_TODO:
+			return [...state.filter(el => el.id !== +payload)]
+		case ACTION_UPDATE_TODO:
+			let newArr = state.map(el => {
+	            if (el.id === +payload) {
+	                el.isDone = !el.isDone
+	            }
+	            return el
+	        })
+
+			return [...newArr]
+		default:
+			return state
+	}
+}
+
+export default rootReducer;
